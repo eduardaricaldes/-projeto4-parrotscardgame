b40d821cde0c1f388eeb9054552ee0892f3cb107
duuda2011@gmail.com <elissonalvessilva@MacBook-Pro-de-Elisson.local>
2022-08-19 17:55:22 -0300

    inicio com java scrips

---
+++ comand.js
@@ -0,0 +1,41 @@
+const gameCards=[
+  {
+    id:1,
+    img:"img/metalparrot.gif",  
+  },
+  {
+    id:2,
+    img:"img/explodyparrot.gif",  
+  },
+  {
+    id:3,
+    img:"img/fiestaparrot.gif",  
+  },
+  {
+    id:4,
+    img:"img/revertitparrot.gif",  
+  },
+  {
+    id:5,
+    img:"img/tripletsparrot.gif",  
+  },
+  {
+
+    id:6,
+    img:"img/unicornparrot.gif",  
+  },
+  {
+    id:7,
+    img:"img/bobrossparrot.gif",  
+  }, 
+  {
+    id:8,
+    img:"img/mustacheparrot.gif",  
+  },
+
+];
+let QuantasCartas=prompt('Com quantas cartas quer jogar?')
+console.log(QuantasCartas);
+
+const game = document.querySelector(".game");
+
